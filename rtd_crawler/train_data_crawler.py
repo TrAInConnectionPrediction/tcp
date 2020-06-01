46db66cae2412d3eb43c641154df27c439cc5dcd
McToel <theo.doellmann@gmx.de>
2020-06-01 15:41:18 +0200

    changed processes to multiprocessing

--- rtd_crawler/train_data_crawler.py
+++ rtd_crawler/train_data_crawler.py
@@ -2,8 +2,8 @@ import sys
 sys.path.append('../')
 import requests
 from rtd_parser import parse_full_day
-from speed import unix_date, unix_now, to_unix
-from downloader import *
+from speed import unix_date, unix_now
+from downloader import DownloadDave
 from helpers import FileLisa, StationPhillip
 import logging.handlers as handlers
 import logging
@@ -12,7 +12,7 @@ import concurrent.futures
 from progress.bar import Bar
 import random
 import datetime
-import os
+from multiprocessing import Process
 
 logger = logging.getLogger('my_app')
 logger.setLevel(logging.INFO)
@@ -32,19 +32,19 @@ def get_station_xml(station_id, str_date, hour, date, station, dd):
 
 
 def get_hourely_batch():
-    stations = StationPhillip()
-    dd = DownloadDave()
-    fl = FileLisa()
-
-    date = unix_date(unix_now())
-    hour = (datetime.datetime.now() + datetime.timedelta(hours=1)).time().hour
-    str_date = datetime.datetime.now().strftime('%y%m%d')
-
-    station_list = list(station for station in stations.random_iter())
-    bar = Bar('crawling ' + str(datetime.datetime.now()), max=len(stations))
-    gatherers = []
-
     with concurrent.futures.ThreadPoolExecutor(max_workers=40) as executor:
+        stations = StationPhillip()
+        dd = DownloadDave()
+        fl = FileLisa()
+
+        date = unix_date(unix_now())
+        hour = (datetime.datetime.now() + datetime.timedelta(hours=1)).time().hour
+        str_date = datetime.datetime.now().strftime('%y%m%d')
+
+        station_list = list(station for station in stations.random_iter())
+        bar = Bar('crawling ' + str(datetime.datetime.now()), max=len(stations))
+        gatherers = []
+
         # start all gathering processes
         for station in station_list:
             station_id = stations.get_eva(name=station)
@@ -63,8 +63,6 @@ def get_hourely_batch():
                     dd.new_ip()
         except concurrent.futures._base.TimeoutError:
             pass
-        except Exception as ex:
-            print(ex)
         executor.shutdown(wait=False)
     bar.finish()
 
@@ -72,43 +70,44 @@ def get_hourely_batch():
 def gather_day(start_hour = 0):
     hour = datetime.datetime.now().time().hour - 1
     last_hour = hour
-    with concurrent.futures.ProcessPoolExecutor() as executor:
-        if start_hour == 0:
-            parser_process = executor.submit(
-                            parse_full_day, datetime.datetime.today() - datetime.timedelta(days=1))
-        while True:
-            if hour == datetime.datetime.now().time().hour:
-                sleep(20)
-            else:
-                hour = datetime.datetime.now().time().hour
-                if last_hour > hour:
-                    break
-                try:
-                    last_hour = datetime.datetime.now().time().hour
 
-                    if 'data_crawler' in locals():
-                        try:
-                            data_crawler.result(timeout=0)
-                        except Exception as ex:
-                            print('crawler error')
-                            logger.exception(ex)
-                    data_crawler = executor.submit(get_hourely_batch)
+    if start_hour == 0:
+        parser_process = Process(target=parse_full_day, args=(datetime.datetime.today() - datetime.timedelta(days=1),))
+        parser_process.start()
+    while True:
+        if hour == datetime.datetime.now().time().hour:
+            sleep(20)
+        else:
+            hour = datetime.datetime.now().time().hour
+            if last_hour > hour:
+                break
+            try:
+                last_hour = datetime.datetime.now().time().hour
 
-                except Exception as ex:
-                    print(ex)
+                if 'data_crawler' in locals():
+                    try:
+                        data_crawler.join(timeout=0)
+                    except Exception as ex:
+                        print('crawler error')
+                        logger.exception(ex)
+                data_crawler = Process(target=get_hourely_batch)
+                data_crawler.start()
+
+            except Exception as ex:
+                print(ex)
+    if 'data_crawler' in locals():
+            try:
+                data_crawler.join(timeout=0)
+            except Exception as ex:
+                print('crawler error')
+                logger.exception(ex)
+
+    if 'parser_process' in locals():
         try:
-            data_crawler.result(timeout=0)
-        except Exception as ex:
-            print('crawler error')
-            logger.exception(ex)
-
-        try:
-            parser_process.result(timeout=0)
+            parser_process.join(timeout=0)
         except Exception as ex:
             print('parser error')
             logger.exception(ex)
-        
-        executor.shutdown(wait=False)
 
 
 if (__name__ == '__main__'):
