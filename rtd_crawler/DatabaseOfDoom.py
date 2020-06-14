b951c659d7b034c3ea3ea9b3f07d54a74a8e5504
McToel <theo.doellmann@gmx.de>
2020-06-14 23:57:09 +0200

    using bulk upsert

--- rtd_crawler/DatabaseOfDoom.py
+++ rtd_crawler/DatabaseOfDoom.py
@@ -1,6 +1,6 @@
 import sqlalchemy
 from sqlalchemy import Column, Integer, Text, DateTime
-from sqlalchemy.dialects.postgresql import JSON
+from sqlalchemy.dialects.postgresql import JSON, insert
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
@@ -11,13 +11,16 @@ from config import db_database, db_password, db_server, db_username
 class DatabaseOfDoom:
     engine = sqlalchemy.create_engine(
             'postgresql://'+ db_username +':' + db_password + '@' + db_server + '/' + db_database + '?sslmode=require',
-            pool_pre_ping=True
+            pool_pre_ping=True,
+            pool_recycle=3600
         )
 
+    Base = declarative_base()
+
     Session = sessionmaker(bind=engine)
+    Base.metadata.create_all(engine)
     session = Session()
 
-    Base = declarative_base()
 
     class JsonRtd(Base):
         __tablename__ = 'json_rtd_v2'
@@ -28,34 +31,53 @@ class DatabaseOfDoom:
 
     Base.metadata.create_all(engine)
 
-    # json_rtd_object = JsonRtd(date=datetime.datetime(2020, 2, 17, 8, 15),
-    #     bhf='Test Hbf',
-    #     plan={'plan':'test'},
-    #     changes={'changes':'test'})
+    queue = []
 
-    # json_rtd_object2 = JsonRtd(date=datetime.datetime(2021, 2, 17, 8, 15),
-    #     bhf='Test Hbf',
-    #     plan={'plan':'test'},
-    #     changes={'changes':'test'})
+    
+    def upsert(self, rows, no_update_cols=[]):
+        table = self.JsonRtd.__table__
 
-    # session.bulk_save_objects([json_rtd_object, json_rtd_object2])
-    # session.commit()
-    # print(session.query(JsonRtd).all())
+        stmt = insert(table).values(rows)
 
-    def __init__(self):
-        self.merges = 0
+        update_cols = [c.name for c in table.c
+                    if c not in list(table.primary_key.columns)
+                    and c.name not in no_update_cols]
+
+        on_conflict_stmt = stmt.on_conflict_do_update(
+            index_elements=table.primary_key.columns,
+            set_={k: getattr(stmt.excluded, k) for k in update_cols}
+            )
+
+        self.session.execute(on_conflict_stmt)
 
     def upload_json(self, plan, changes, bhf, date, hour):
         date = datetime.datetime.combine(date, datetime.time(hour, 0))
-        new_row = self.JsonRtd(date=date, bhf=bhf, plan=plan, changes=changes)
-        self.session.merge(new_row)
+        # new_row = self.JsonRtd(date=date, bhf=bhf, plan=plan, changes=changes)
+        self.queue.append({'date': date, 'bhf': bhf, 'plan': plan, 'changes':changes})
+        if len(self.queue) > 20:
+            self.commit()
+            # self.session.bulk_update_mappings(self.JsonRtd, self.queue)
+            # self.queue = []
+            # self.session.commit()
+        # for _i in range(3):
+        #     try:
+        #         self.session.merge(new_row)
+        #         self.session.commit()
+        #         break
+        #     except:
+        #         print('rollback')
+        #         self.session.rollback()
 
     def commit(self):
+        self.upsert(self.queue)
+        # self.session.bulk_update_mappings(self.JsonRtd, self.queue)
+        self.queue = []
         self.session.commit()
 
     def get_json(self, bhf, date):
-        result = self.session.query(self.JsonRtd).filter(self.JsonRtd.bhf == bhf & self.JsonRtd.date == date).all()
-        print(result)
+        if date is None:
+            return self.session.query(self.JsonRtd).filter((self.JsonRtd.bhf == bhf)).all()
+        return self.session.query(self.JsonRtd).filter((self.JsonRtd.bhf == bhf) & (self.JsonRtd.date == date)).first()
 
     def count_entrys_at_date(self, date):
         return self.session.query(self.JsonRtd).filter(self.JsonRtd.date == date).count()
@@ -64,7 +86,3 @@ class DatabaseOfDoom:
 if __name__ == "__main__":
     db = DatabaseOfDoom()
     print(db.count_entrys_at_date(datetime.datetime(2020, 6, 11, 17)))
-    # result = db.session.query(db.JsonRtd).first()
-    # print(result)
-    # # db.get_json('Tübingen Hbf', None)
-    # print('lol')
--- rtd_crawler/train_data_crawler.py
+++ rtd_crawler/train_data_crawler.py
@@ -1,4 +1,4 @@
-from multiprocessing import Process
+from multiprocessing import Process, Pool
 import datetime
 import random
 import progressbar
@@ -61,7 +61,7 @@ def get_station_json(station_id, str_date, hour, station, dd):
     return {'plan': plan_json, 'changes': changes_json, 'station': station}
 
 
-def get_hourely_batch():
+def get_hourely_batch(_lol):
     """gathers plan and real xmls of the current date and hour from all stations
     inside of the stations table of the db. This function should be called once every hour.
     """
@@ -83,21 +83,23 @@ def get_hourely_batch():
             station_id = stations.get_eva(name=station)
             gatherers.append(executor.submit(get_station_json, station_id, str_date, hour, station, dd))
         try:
-            with progressbar.ProgressBar(max_value=len(stations)) as bar:
-                # collect all finished gathering threads while changing the ip
-                for i, gatherer in enumerate(concurrent.futures.as_completed(gatherers, timeout=(60*55))):
-                    jsons = gatherer.result()
+            bar = progressbar.ProgressBar(max_value=len(stations)).start()
+            # with progressbar.ProgressBar(max_value=len(stations)) as bar:
+            # collect all finished gathering threads while changing the ip
+            for i, gatherer in enumerate(concurrent.futures.as_completed(gatherers, timeout=(60*55))):
+                jsons = gatherer.result()
 
-                    db.upload_json(jsons['plan'], jsons['changes'], jsons['station'], datetime_date, hour)
-                    if not i % 15:
-                        db.commit()
-                    
-                    bar.update(i)
+                db.upload_json(jsons['plan'], jsons['changes'], jsons['station'], datetime_date, hour)
+                # if not i % 15:
+                #     db.commit()
+                
+                bar.update(i)
 
-                    # change ip in average each 400th time
-                    if random.randint(-200, 200) == 0:
-                        dd.new_ip()
-            db.commit()
+                # change ip in average each 400th time
+                if random.randint(-200, 200) == 0:
+                    dd.new_ip()
+            bar.finish()
+            # db.commit()
         except concurrent.futures._base.TimeoutError:
             pass
         executor.shutdown(wait=False)
@@ -108,21 +110,23 @@ if (__name__ == '__main__'):
     import fancy_print_tcp
     hour = datetime.datetime.now().time().hour - 1
 
-    while True:
-        if hour == datetime.datetime.now().time().hour:
-            sleep(20)
-        else:
-            hour = datetime.datetime.now().time().hour
-            try:
-                if 'data_crawler' in locals():
-                    try:
-                        data_crawler.join(timeout=0)
-                    except Exception as ex:
-                        print('crawler error')
-                        logger.exception(ex)
-                data_crawler = Process(target=get_hourely_batch)
-                data_crawler.start()
+    with Pool(processes=1, maxtasksperchild=1) as pool:
+        while True:
+            if hour == datetime.datetime.now().time().hour:
+                sleep(20)
+            else:
+                hour = datetime.datetime.now().time().hour
+                try:
+                    pool.map(get_hourely_batch, range(1))
+                    # if 'data_crawler' in locals():
+                    #     try:
+                    #         data_crawler.join(timeout=0)
+                    #     except Exception as ex:
+                    #         print('crawler error')
+                    #         logger.exception(ex)
+                    # data_crawler = Process(target=get_hourely_batch)
+                    # data_crawler.start()
 
-            except Exception as ex:
-                print(ex)
-                logger.exception(ex)
+                except Exception as ex:
+                    print(ex)
+                    logger.exception(ex)
