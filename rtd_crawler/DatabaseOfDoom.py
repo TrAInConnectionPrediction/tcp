089823011fdec2a4e2f1b557b9583b73c9a4b514
McToel <theo.doellmann@gmx.de>
2020-06-20 16:47:38 +0200

    added new parser

--- rtd_crawler/DatabaseOfDoom.py
+++ rtd_crawler/DatabaseOfDoom.py
@@ -1,5 +1,5 @@
 import sqlalchemy
-from sqlalchemy import Column, Integer, Text, DateTime
+from sqlalchemy import Column, Integer, Text, DateTime, String
 from sqlalchemy.dialects.postgresql import JSON, insert
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
@@ -29,6 +29,59 @@ class DatabaseOfDoom:
         plan = Column(JSON)
         changes = Column(JSON)
 
+    class Rtd(Base):
+            __tablename__ = 'rtd'
+            ar_ppth = Column(Text)
+            ar_cpth = Column(Text)
+            ar_pp = Column(Text)
+            ar_cp = Column(Text)
+            ar_pt = Column(Integer)
+            ar_ct = Column(Integer)
+            ar_ps = Column(String(length=1))
+            ar_cs = Column(String(length=1))
+            ar_hi = Column(Integer)
+            ar_clt = Column(Integer)
+            ar_wings = Column(Text)
+            ar_tra = Column(Text)
+            ar_pde = Column(Text)
+            ar_cde = Column(Text)
+            ar_dc = Column(Integer)
+            ar_l = Column(Text)
+            ar_m = Column(JSON)
+
+            dp_ppth = Column(Text)
+            dp_cpth = Column(Text)
+            dp_pp = Column(Text)
+            dp_cp = Column(Text)
+            dp_pt = Column(Integer)
+            dp_ct = Column(Integer)
+            dp_ps = Column(String(length=1))
+            dp_cs = Column(String(length=1))
+            dp_hi = Column(Integer)
+            dp_clt = Column(Integer)
+            dp_wings = Column(Text)
+            dp_tra = Column(Text)
+            dp_pde = Column(Text)
+            dp_cde = Column(Text)
+            dp_dc = Column(Integer)
+            dp_l = Column(Text)
+            dp_m = Column(JSON)
+
+            f = Column(String(length=1))
+            t = Column(Text)
+            o = Column(Text)
+            c = Column(Text)
+            n = Column(Text)
+
+            m = Column(JSON)
+            hd = Column(JSON)
+            hdc = Column(JSON)
+            conn = Column(JSON)
+            rtr = Column(JSON)
+
+            station = Column(Text)
+            id = Column(Text, primary_key=True)
+
     Base.metadata.create_all(engine)
 
     queue = []
@@ -52,7 +105,6 @@ class DatabaseOfDoom:
 
     def add_row(self, plan, changes, bhf, date, hour):
         date = datetime.datetime.combine(date, datetime.time(hour, 0))
-        # new_row = self.JsonRtd(date=date, bhf=bhf, plan=plan, changes=changes)
         self.queue.append({'date': date, 'bhf': bhf, 'plan': plan, 'changes':changes})
         if len(self.queue) > 20:
             self.commit()
@@ -71,15 +123,23 @@ class DatabaseOfDoom:
         self.queue = []
         self.session.commit()
 
-    def get_json(self, bhf, date):
-        if date is None:
+    def get_json(self, bhf, date1, date2):
+        if date1 is None:
             return self.session.query(self.JsonRtd).filter((self.JsonRtd.bhf == bhf)).all()
-        return self.session.query(self.JsonRtd).filter((self.JsonRtd.bhf == bhf) & (self.JsonRtd.date == date)).first()
+        if date2 is None:
+            return self.session.query(self.JsonRtd).filter((self.JsonRtd.bhf == bhf) & (self.JsonRtd.date == date1)).first()
+        return self.session.query(self.JsonRtd).filter((self.JsonRtd.bhf == bhf)
+            & (self.JsonRtd.date >= date1)
+            & (self.JsonRtd.date < date2)).all()
 
     def count_entrys_at_date(self, date):
         return self.session.query(self.JsonRtd).filter(self.JsonRtd.date == date).count()
 
+    def max_date(self):
+        return self.session.query(sqlalchemy.func.max(self.Rtd.ar_pt)).scalar()
+
 
 if __name__ == "__main__":
     db = DatabaseOfDoom()
+    print(db.max_date())
     print(db.count_entrys_at_date(datetime.datetime(2020, 6, 11, 17)))
---
+++ rtd_crawler/data_parser.py
@@ -0,0 +1,193 @@
+import pandas as pd 
+import pangres
+import numpy as np 
+import sqlalchemy
+from sqlalchemy import Column, Integer, Text, DateTime, String, Boolean
+from sqlalchemy.dialects.postgresql import JSON, insert
+import datetime
+import progressbar
+import json
+from helpers import StationPhillip
+from DatabaseOfDoom import DatabaseOfDoom
+import pickle
+
+from config import db_database, db_password, db_server, db_username
+
+
+sql_types = {
+    'ar_ppth': Text,
+    'ar_cpth': Text,
+    'ar_pp': Text,
+    'ar_cp': Text,
+    'ar_pt': Integer,
+    'ar_ct': Integer,
+    'ar_ps': String(length=1),
+    'ar_cs': String(length=1),
+    'ar_hi': Integer,
+    'ar_clt': Integer,
+    'ar_wings': Text,
+    'ar_tra': Text,
+    'ar_pde': Text,
+    'ar_cde': Text,
+    'ar_dc': Integer,
+    'ar_l': Text,
+    'ar_m': JSON,
+
+    'dp_ppth': Text,
+    'dp_cpth': Text,
+    'dp_pp': Text,
+    'dp_cp': Text,
+    'dp_pt': Integer,
+    'dp_ct': Integer,
+    'dp_ps': String(length=1),
+    'dp_cs': String(length=1),
+    'dp_hi': Integer,
+    'dp_clt': Integer,
+    'dp_wings': Text,
+    'dp_tra': Text,
+    'dp_pde': Text,
+    'dp_cde': Text,
+    'dp_dc': Integer,
+    'dp_l': Text,
+    'dp_m': JSON,
+
+    'f': String(length=1),
+    't': Text,
+    'o': Text,
+    'c': Text,
+    'n': Text,
+
+    'm': JSON,
+    'hd': JSON,
+    'hdc': JSON,
+    'conn': JSON,
+    'rtr': JSON,
+
+    'station': Text,
+    'id': Text
+}
+
+def parse_stop_plan(stop):
+    if 'tl' in stop:
+        for key in stop['tl'][0]:
+            stop[key] = stop['tl'][0][key]
+        stop.pop('tl')
+    if 'ar' in stop:
+        for key in stop['ar'][0]:
+            stop['ar_' + key] = stop['ar'][0][key]
+        stop.pop('ar')
+    if 'dp' in stop:
+        for key in stop['dp'][0]:
+            stop['dp_' + key] = stop['dp'][0][key]
+        stop.pop('dp')
+    return stop
+
+
+def add_change_to_stop(stop, change):
+    if 'ar' in change:
+        for key in change['ar'][0]:
+            stop['ar_' + key] = change['ar'][0][key]
+    if 'dp' in change:
+        for key in change['dp'][0]:
+            stop['dp_' + key] = change['dp'][0][key]
+    if 'm' in change:
+        stop['m'] = change['m']
+    return stop
+
+
+def parse_station(station_data):
+    parsed = []
+    station_data = {hour_batch.date: hour_batch for hour_batch in station_data}
+    for date in station_data:
+        plan = station_data[date].plan
+        if plan is None:
+            continue
+        for stop in plan:
+            stop = parse_stop_plan(stop)
+            for changes_delta in range(3, -1, -1):
+                try:
+                    changes = station_data[date + datetime.timedelta(hours=changes_delta)].changes
+                except KeyError:
+                    continue
+                if changes is None:
+                    continue
+                # check wether the stop is still in the next plan. This happens when the train has delay and so its arr/dep is actually in the next hour
+                # we cannot have a stop twice in our database.
+                try:
+                    if changes_delta > 0:
+                        next_plan = station_data[date + datetime.timedelta(hours=changes_delta)].plan
+                        if next_plan is not None:
+                            if stop['id'] in (next_stop['id'] for next_stop in next_plan):
+                                stop = None
+                                break
+                except KeyError:
+                    pass
+                for change in changes:
+                    if stop['id'] == change['id']:
+                        stop = add_change_to_stop(stop, change)
+                        break
+            if stop:
+                parsed.append(stop)
+    return parsed
+
+
+def upsert_rtd(table, conn, keys, data_iter):
+    rows = []
+    table = db.Rtd.__table__
+
+    stmt = insert(table).values(rows)
+
+    update_cols = [c.name for c in table.c
+                if c not in list(table.primary_key.columns)]
+
+    on_conflict_stmt = stmt.on_conflict_do_update(
+        index_elements=table.primary_key.columns,
+        set_={k: getattr(stmt.excluded, k) for k in update_cols}
+        )
+
+    conn.execute(on_conflict_stmt)
+
+
+def upload_data(df):
+    """This function uploads the data to our database
+
+    Arguments:
+        df {pd.DataFrame} -- parsed data
+    """
+    pass
+    # df = df.set_index('id')
+    # pangres.upsert(engine, df, if_row_exists='update', table_name='rtd')
+    # # df = df.set_index('id')
+    # df.to_sql('rtd', con=engine, if_exists='append', method=upsert_rtd, dtype=sql_types)
+
+
+if __name__ == "__main__":
+    start_date = datetime.datetime(2020, 1, 1, 0, 0)
+    end_date = datetime.datetime.now()
+
+    engine = sqlalchemy.create_engine('postgresql://'+ db_username +':' + db_password + '@' + db_server + '/' + db_database + '?sslmode=require') 
+    stations = StationPhillip()
+    db = DatabaseOfDoom()
+    buffer = pd.DataFrame()
+    with progressbar.ProgressBar(max_value=len(stations)) as bar:
+        for i, station in enumerate(stations):
+            station_data = {}
+            station_data = db.get_json(station, date1=start_date, date2=end_date)
+            parsed = parse_station(station_data)
+            parsed = pd.DataFrame(parsed) # .drop_duplicates(subset=['id'], keep='last')
+            parsed['station'] = station
+            buffer = pd.concat([buffer, parsed], ignore_index=True)
+
+            # upload the data as soon as it is longer than 1000 rows. This is more efficient than uploading each stations data individually
+            if len(buffer) > 1000:
+                upload_data(buffer)
+                buffer = pd.DataFrame()
+            bar.update(i)
+            # download jsons
+            # open plan json
+            # open real jsons
+            # iterate over plan json
+                # find newest changed event
+                # add it to plan
+                # break
+        
