3040498e004de32f160fe95df2966f1bdc172827
McToel <theo.doellmann@gmx.de>
2020-08-29 16:54:26 +0200

    Updated style and color of fancy_print_tcp
    Signed-off-by: McToel <theo.doellmann@gmx.de>

--- fancy_print_tcp.py
+++ fancy_print_tcp.py
@@ -1,13 +1,13 @@
-# This module is used to print this fancy thing. Just do import tcp for that
+# This module is used to print this fancy thing. Just do import fancy_print_tcp for that
 
-print("""
+TCP_SING = """
                             ╔═══╗                         
    ╔════════════════════════╩═══╩════════════════════════╗
-   ║           ██████████  ████████  ███████             ║
-   ║               ██      ██        ██     ██           ║
-   ║               ██      ██        ███████             ║
+   ║            ████████    ██████   ██████              ║
+   ║               ██      ██        ██   ██             ║
+   ║               ██      ██        ██████              ║
    ║               ██      ██        ██                  ║
-   ║               ██      ████████  ██                  ║
+   ║               ██       ██████   ██                  ║
    ╚════════════════════════╦═══╦════════════════════════╝
                     \\''''───║   ╟───''''/                
                      )__,--─║   ╟─--,__(                  
@@ -16,4 +16,6 @@ print("""
 TrAIn_Connection_Prediction ║   ║                         
           Service           ║   ║                         
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^╜   ╙^^^^^^^^^^^^^^^^^^^^^^^^^^^
-""")
+"""
+
+print("\x1b[3;34m{}\x1b[0m".format(TCP_SING))
