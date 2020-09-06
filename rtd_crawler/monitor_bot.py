822c5585f4501af70c2758914bb3af1695cc10c4
McToel <theo.doellmann@gmx.de>
2020-09-06 16:23:47 +0200

    Fix: monitor can now send messages
    Signed-off-by: McToel <theo.doellmann@gmx.de>

--- rtd_crawler/monitor_bot.py
+++ rtd_crawler/monitor_bot.py
@@ -23,6 +23,42 @@ async def on_ready():
     await channel.send('Data gatherer monitor now active')
 
 
+@client.event
+async def monitor_hour():
+    channel = client.get_channel(720671295129518232)
+    # channel = discord.Object(id=720671295129518232)
+    hour = datetime.datetime.now().time().hour - 1
+    old_change_count = 0
+
+    hour = datetime.datetime.now().time().hour
+    date_to_check = datetime.datetime.combine(datetime.date.today(),
+                                              datetime.time(hour, 0)) - datetime.timedelta(hours=1)
+    new_row_cont = db.count_entries_at_date(date_to_check)
+    if new_row_cont < 7000:
+        message = '@everyone The gatherer is not working, as {} new entries where added to database at {}'\
+                  .format(str(new_row_cont), str(date_to_check))
+        await channel.send(message)
+    print('checked ' + str(date_to_check) + ': ' + str(new_row_cont) + ' rows were added')
+
+    plan_row_count = plan.count_entries_at_date(date_to_check)
+    if plan_row_count < 7000:
+        message = '@everyone The plan gatherer is not working, as {} new entries where added to database at {}'\
+                  .format(str(plan_row_count), str(date_to_check))
+        await channel.send(message)
+    print('checked plan ' + str(date_to_check) + ': ' + str(plan_row_count) + ' rows were added')
+
+    if hour == 6:
+        new_change_count = changes.count_entries(date_to_check)
+        count_delta = new_change_count - old_change_count
+        if count_delta < 50000:
+            message = '''@everyone The recent change gatherer is not working, as {} 
+                      new entries where added to database at {}'''\
+                      .format(str(count_delta), str(date_to_check))
+            await channel.send(message)
+        old_change_count = new_change_count
+        print('checked changes ' + str(date_to_check) + ': ' + str(count_delta) + ' rows were added')
+
+
 class Monitor(commands.Cog):
     def __init__(self):
         self.monitor.start()
@@ -33,37 +69,7 @@ class Monitor(commands.Cog):
     @tasks.loop(hours=1)
     async def monitor(self):
         await client.wait_until_ready()
-        channel = discord.Object(id=720671295129518232)
-        hour = datetime.datetime.now().time().hour - 1
-        old_change_count = 0
-
-        hour = datetime.datetime.now().time().hour
-        date_to_check = datetime.datetime.combine(datetime.date.today(),
-                                                  datetime.time(hour, 0)) - datetime.timedelta(hours=1)
-        new_row_cont = db.count_entries_at_date(date_to_check)
-        if new_row_cont < 7000:
-            message = '@everyone The gatherer is not working, as {} new entries where added to database at {}'\
-                      .format(str(new_row_cont), str(date_to_check))
-            await client.send_message(channel, message)
-        print('checked ' + str(date_to_check) + ': ' + str(new_row_cont) + ' rows were added')
-
-        plan_row_count = plan.count_entries_at_date(date_to_check)
-        if plan_row_count < 7000:
-            message = '@everyone The plan gatherer is not working, as {} new entries where added to database at {}'\
-                      .format(str(plan_row_count), str(date_to_check))
-            await client.send_message(channel, message)
-        print('checked plan ' + str(date_to_check) + ': ' + str(plan_row_count) + ' rows were added')
-
-        if hour == 6:
-            new_change_count = changes.count_entries(date_to_check)
-            count_delta = new_change_count - old_change_count
-            if count_delta < 50000:
-                message = '''@everyone The recent change gatherer is not working, as {} 
-                          new entries where added to database at {}'''\
-                          .format(str(count_delta), str(date_to_check))
-                await client.send_message(channel, message)
-            old_change_count = new_change_count
-            print('checked changes ' + str(date_to_check) + ': ' + str(count_delta) + ' rows were added')
+        await monitor_hour()
 
 
 if __name__ == "__main__":
