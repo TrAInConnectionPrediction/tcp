ce8f692dcd6fba45dd44b84676d2ae7d759d57e1
kuthy <mariusdkm@gmail.com>
2020-09-16 15:35:41 +0200

    some minor tweaking

--- server/restart.sh
+++ server/restart.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-export PATH=/usr/bin
-
 echo restarting the webserver
-sudo systemctl restart webserver
+sudo /bin/systemctl restart webserver
--- setup.sh
+++ setup.sh
@@ -37,7 +37,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 dest="/etc/systemd/system/webserver.service"
 
 sudo sed "s|SOURCE_PATH|${SCRIPT_DIR}|g" server/webserver.service > ${dest}
-sudo sed -i "s|SERVER_IP|${1:-"10.16.1.200:5000"}|g" ${dest}
+sudo sed -i "s|SERVER_IP|${1:-"127.0.0.1:5000"}|g" ${dest}
 sudo sed -i "s|USERNAME|$(logname)|g" ${dest}
 
 sudo systemctl daemon-reload
