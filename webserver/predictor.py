499be91bd24b44fe69d1875882dc806c217470e8
McToel <theo.doellmann@gmx.de>
2020-12-30 22:03:23 +0100

    now initiating all classes in __init__.py

--- webserver/predictor.py
+++ webserver/predictor.py
@@ -4,7 +4,7 @@ import pandas as pd
 import datetime
 import numpy as np
 from pytz import timezone
-from helpers.StreckennetzSteffi import StreckennetzSteffi
+from webserver import streckennetz
 
 
 def from_unix(unix):
@@ -26,7 +26,7 @@ def from_unix(unix):
 
 class Predictor:
     def __init__(self):
-        self.streckennetz = StreckennetzSteffi()
+        # self.streckennetz = StreckennetzSteffi()
         self.cat_encoders = {}
         for cat in ["o", "c", "n", "station"]:
             self.cat_encoders[cat] = pickle.load(
@@ -79,7 +79,7 @@ class Predictor:
         if transfer_time > 39:
             return 1, ar_prediction[5], dp_prediction[5]
         con_score = ar_prediction[transfer_time - 2] * dp_prediction[0]
-        con_score += np.sum((ar_prediction[transfer_time-1:] - ar_prediction[transfer_time-2:-1]) * dp_prediction[1:-transfer_time + 2])
+        con_score += np.sum((ar_prediction[transfer_time-1:] - ar_prediction[transfer_time-2:-1]) * dp_prediction[1:2-transfer_time or len(dp_prediction)])
         return con_score, ar_prediction[5], dp_prediction[5]
 
         # pred1 = self.predict(data1, 1)
@@ -124,27 +124,27 @@ class Predictor:
                 ]
             )
             # departure of segment
-            data.loc[0, "station"] = self.streckennetz.get_name(eva=int(connection["segmentStart"]["id"]))
+            data.loc[0, "station"] = streckennetz.get_name(eva=int(connection["segmentStart"]["id"]))
             trip = connection["full_trip"][
                 : connection["full_trip"].index(
-                    self.streckennetz.get_eva(name=data.at[0, "station"])
+                    int(connection["segmentStart"]["id"])
                 )
                 + 1
             ]
             if len(trip) == 1:
                 data.at[0, "distance_to_start"] = 0
             else:
-                data.at[0, "distance_to_start"] = self.streckennetz.eva_route_length(trip)
+                data.at[0, "distance_to_start"] = streckennetz.eva_route_length(trip)
     
             trip = connection["full_trip"][
                 connection["full_trip"].index(
-                    self.streckennetz.get_eva(name=data.at[0, "station"])
+                    streckennetz.get_eva(name=data.at[0, "station"])
                 ) :
             ]
             if len(trip) == 1:
                 data.at[0, "distance_to_end"] = 0
             else:
-                data.at[0, "distance_to_end"] = self.streckennetz.eva_route_length(trip)
+                data.at[0, "distance_to_end"] = streckennetz.eva_route_length(trip)
 
             time = from_unix(connection["departure"]["time"])
             data.at[0, "hour"] = time.hour
@@ -165,35 +165,41 @@ class Predictor:
                 data.at[0, "o"] = 0
     
             # arrival of segment
-            data.loc[1, "station"] = self.streckennetz.get_name(
+            data.loc[1, "station"] = streckennetz.get_name(
                 eva=int(connection["segmentDestination"]["id"])
             )
             trip = connection["full_trip"][
                 : connection["full_trip"].index(
-                    self.streckennetz.get_eva(name=data.at[1, "station"])
+                    int(connection["segmentDestination"]["id"])
                 )
                 + 1
             ]
             if len(trip) == 1:
                 data.at[1, "distance_to_start"] = 0
             else:
-                data.at[1, "distance_to_start"] = self.streckennetz.eva_route_length(trip)
+                data.at[1, "distance_to_start"] = streckennetz.eva_route_length(trip)
     
             trip = connection["full_trip"][
                 connection["full_trip"].index(
-                    self.streckennetz.get_eva(name=data.at[1, "station"])
+                    int(connection["segmentDestination"]["id"])
                 ) :
             ]
             if len(trip) == 1:
                 data.at[1, "distance_to_end"] = 0
             else:
-                data.at[1, "distance_to_end"] = self.streckennetz.eva_route_length(trip)
+                data.at[1, "distance_to_end"] = streckennetz.eva_route_length(trip)
             time = from_unix(connection["arrival"]["time"])
             data.at[1, "hour"] = time.hour
             data.at[1, "day"] = time.weekday()
-            data.at[0, "station"] = self.cat_encoders["station"][data.at[0, "station"]]
-            data.at[1, "station"] = self.cat_encoders["station"][data.at[1, "station"]]
-    
+            try:
+                data.at[0, "station"] = self.cat_encoders["station"][data.at[0, "station"]]
+            except KeyError:
+                data.at[0, "station"] = 0
+            try:
+                data.at[1, "station"] = self.cat_encoders["station"][data.at[1, "station"]]
+            except KeyError:
+                data.at[1, "station"] = 0
+
             try:
                 data.at[1, "c"] = self.cat_encoders["c"][connection["train"]["type"]]
             except KeyError:
