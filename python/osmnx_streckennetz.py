67688005ebaa8e098e1b9aa6b4ac5617b3216185
McToel <theo.doellmann@gmx.de>
2021-01-09 04:24:18 +0100

    Updated streckennetz to use osmnx 1.0 and performance improvements

--- python/osmnx_streckennetz.py
+++ python/osmnx_streckennetz.py
@@ -1,17 +1,20 @@
-import os, sys
+import os
+import sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import osmnx as ox
 import networkx as nx
 import shapely
-from shapely.geometry import Point, LineString
+from shapely.geometry import Point, LineString, Polygon
 import numpy as np
 import geopy.distance
-# import matplotlib.pyplot as plt
 import itertools
 import progressbar
 import pandas as pd
 import geopandas as gpd
 from helpers.StationPhillip import StationPhillip
+import matplotlib.pyplot as plt
+import pickle
+from concurrent.futures import ProcessPoolExecutor
 
 
 def get_streckennetz_from_osm():
@@ -23,14 +26,18 @@ def get_streckennetz_from_osm():
     nx.Graph:
         Streckennetz without stations.
     """
-    ox.config(log_console=True, use_cache=True)
-    rail_filter = '["railway"~"rail|tram|narrow_gauge|light_rail"]'
-    streckennetz = ox.graph_from_place('Germany',
-                                       simplify=True,
-                                       network_type='none',
-                                       custom_filter=rail_filter
-                                       )
-    # nx.write_gpickle(streckennetz, "data_buffer/original_osm_rail_graph.gpickle")
+    try:
+        streckennetz = nx.read_gpickle("cache/original_osm_rail_graph.gpickle")
+        print('Using chached streckennetz insted of downloading new one from osm')
+    except FileNotFoundError:
+        ox.config(log_console=True, use_cache=True)
+        rail_filter = '["railway"~"rail|tram|narrow_gauge|light_rail"]'
+        streckennetz = ox.graph_from_place('Germany',
+                                        simplify=True,
+                                        network_type='none',
+                                        custom_filter=rail_filter
+                                        )
+        nx.write_gpickle(streckennetz, "cache/original_osm_rail_graph.gpickle")
     return streckennetz
 
 
@@ -44,7 +51,7 @@ def to_geographic_coordinate_system(line):
     return LineString((np.array(tuple(zip(line.xy[0], line.xy[1]))) / 1e6))
 
 
-def find_edges_to_split(bhf: str, nearest_edge: list, streckennetz, nodes, edges, stations):
+def find_edges_to_split(bhf: str, nearest_edge: list, streckennetz, edges, stations, plot=False):
     """
     Find edges that are part of a station by setting a cross at the coordinates of a station and finding intersections
     with Streckennetz edges.
@@ -55,8 +62,6 @@ def find_edges_to_split(bhf: str, nearest_edge: list, streckennetz, nodes, edges
         Station to find.
     streckennetz: nx.Graph
         Graph of Streckennetz to find the station in.
-    nodes: gpd.GeoDataFrame
-        Nodes of Streckennetz.
     edges: gpd.GeoDataFrame
         Edges of Streckennetz.
     stations: StationPhillip
@@ -92,22 +97,26 @@ def find_edges_to_split(bhf: str, nearest_edge: list, streckennetz, nodes, edges
     bbox = shapely.geometry.box(original_coords[0] - 0.001, original_coords[1] - 0.001,
                                 original_coords[0] + 0.001, original_coords[1] + 0.001)
     join_gdf = gpd.GeoDataFrame({'geometry': [bbox]}, crs=edges.crs)
-    close_edges = edges.loc[gpd.sjoin(join_gdf, edges, how='inner')['index_right'].to_list()]
+    close_edges = edges.loc[gpd.sjoin(join_gdf, edges, how='inner')['index_right0'].unique()]
 
-    # Plot Station
-    # fig, ax = plt.subplots(figsize=(50,50))
-    # ax.set_aspect('equal', 'datalim')
-    #
-    # for index, edge in close_edges.iterrows():
-    #     geom = reduce_decimal_precision(edge['geometry'])
-    #     # xs, ys = geom.exterior.xy
-    #     # ax.fill(xs, ys, alpha=1, fc='r', ec='none')
-    #     ax.plot(*geom.xy)
-    #
-    # # ax.plot(*line1.xy)
-    # # ax.plot(*line2.xy)
+    if plot:
+        # Plot Station
+        fig, ax = plt.subplots(figsize=(50,50))
+        ax.set_aspect('equal', 'datalim')
+        
+        for index, edge in close_edges.iterrows():
+            geom = reduce_decimal_precision(edge['geometry'])
+            # xs, ys = geom.exterior.xy
+            # ax.fill(xs, ys, alpha=1, fc='r', ec='none')
+            ax.plot(*geom.xy, color='black')
+        
+        ax.plot(*line1.xy, color='red')
+        ax.plot(*line2.xy, color='red')
+        # ax.scatter(*coords)
+        ax.scatter(*points[0].xy, color='blue')
+        ax.scatter(*points[1].xy, color='blue')
 
-    # plt.show()
+        plt.show()
 
     edges_split = []
     for index, edge in close_edges.iterrows():
@@ -145,7 +154,10 @@ def length_of_line(line) -> int:
     int:
         Length of line.
     """
-    return sum((geopy.distance.distance(p1, p2).meters for p1, p2 in pairwise(tuple(zip(line.xy[0], line.xy[1])))))
+    if line is not None:
+        return sum((geopy.distance.distance(p1, p2).meters for p1, p2 in pairwise(tuple(zip(line.xy[0], line.xy[1])))))
+    else:
+        return None
 
 
 def split_edges(to_split: list):
@@ -166,27 +178,37 @@ def split_edges(to_split: list):
         List of edges to add.
 
     """
-    nodes_to_add = {}
+    nodes_to_add = []
     edges_to_remove = []
     edges_to_add = []
+    bhfs_added = []
     for bhf, edge, splitter in to_split:
-        nodes_to_add[bhf] = {**dict(zip(('x', 'y'), stations.get_location(name='Tübingen Hbf'))), 'bhf': True}
+        if not bhf in bhfs_added:
+            nodes_to_add.append([bhf, {**dict(zip(('x', 'y'), stations.get_location(name=bhf))), 'bhf': True}])
+            bhfs_added.append(bhf)
         edges_to_remove.append((edge['u'], edge['v']))
         geom = reduce_decimal_precision(edge['geometry'])
         geoms = shapely.ops.split(geom, splitter)
-        geoms = [to_geographic_coordinate_system(geoms[0]),
-                 to_geographic_coordinate_system(geoms[1])]
-        edges_to_add.append((edge['u'], bhf, {
-            'geometry': geoms[0],
-            'length': length_of_line(geoms[0]),
-            **{key: value for key, value in edge.items() if key not in ('u', 'v', 'length', 'geometry')}
-        }))
-        edges_to_add.append((bhf, edge['v'], {
-            'geometry': geoms[1],
-            'length': length_of_line(geoms[1]),
-            **{key: value for key, value in edge.items() if key not in ('u', 'v', 'length', 'geometry')}
-        }))
-    return nodes_to_add.items(), edges_to_remove, edges_to_add
+        if len(geoms) < 2:
+            pass
+            # print('len(geoms) < 2:', bhf, edge['u'], edge['v'])
+        elif len(geoms) > 2:
+            pass
+            # print('len(geoms) > 2:', bhf, edge['u'], edge['v'])
+        else:
+            geoms = [to_geographic_coordinate_system(geoms[0]),
+                    to_geographic_coordinate_system(geoms[1])]
+            edges_to_add.append((edge['u'], bhf, {
+                'geometry': geoms[0],
+                'length': length_of_line(geoms[0]),
+                **{key: value for key, value in edge.items() if key not in ('u', 'v', 'length', 'geometry')}
+            }))
+            edges_to_add.append((bhf, edge['v'], {
+                'geometry': geoms[1],
+                'length': length_of_line(geoms[1]),
+                **{key: value for key, value in edge.items() if key not in ('u', 'v', 'length', 'geometry')}
+            }))
+    return (nodes_to_add, edges_to_remove, edges_to_add)
 
 
 def upload_minimal(streckennetz):
@@ -200,7 +222,12 @@ def upload_minimal(streckennetz):
     """
     from database.engine import engine
     streckennetz = ox.graph_to_gdfs(streckennetz, nodes=False)
+    u_v_key = pd.DataFrame(streckennetz.index.to_numpy().tolist(), columns=streckennetz.index.names)
+    u_v_key = u_v_key.set_index(['u', 'v', 'key'], drop=False)
+    streckennetz['u'] = u_v_key['u']
+    streckennetz['v'] = u_v_key['v']
     streckennetz = pd.DataFrame(streckennetz[['u', 'v', 'length']])
+    streckennetz = streckennetz.reset_index(drop=True)
     streckennetz.to_sql('minimal_streckennetz', if_exists='replace', method='multi', con=engine)
 
 
@@ -210,51 +237,81 @@ if __name__ == '__main__':
 
     ox.config(log_console=True, use_cache=True)
     streckennetz = get_streckennetz_from_osm()
-    # streckennetz = nx.read_gpickle("data_buffer/original_osm_rail_graph.gpickle")
     s_nodes, s_edges = ox.graph_to_gdfs(streckennetz, fill_edge_geometry=True)
+    u_v_key = pd.DataFrame(s_edges.index.to_numpy().tolist(), columns=s_edges.index.names)
+    u_v_key = u_v_key.set_index(['u', 'v', 'key'], drop=False)
+    s_edges['u'] = u_v_key['u']
+    s_edges['v'] = u_v_key['v']
+    streckennetz = ox.graph_from_gdfs(s_nodes, s_edges)
     s_edges.sindex
 
     # Calculate nearest edge for each station
     print('Calculating nearest edges')
-    x = []
-    y = []
-    for station in stations:
-        coords = stations.get_location(name=station)
-        x.append(coords[0])
-        y.append(coords[1])
+    # Project the coordinates of the stations (using a collection
+    # of points makes it faster than projecting it one by one)
+    point_collection = LineString([[*stations.get_location(name=station)] for station in stations])
+    point_collection = ox.projection.project_geometry(point_collection)[0].xy
+    x = point_collection[0]
+    y = point_collection[1]
+
     streckennetz_projected = ox.project_graph(streckennetz)
     nearest_edges = ox.get_nearest_edges(streckennetz_projected, x, y, method='kdtree', dist=50).tolist()
+    pickle.dump(nearest_edges, open("cache/streckennetz.pkl", "wb"))
+    # nearest_edges = pickle.load(open("cache/streckennetz.pkl", "rb"))
 
     # Find edges where a station node should be inserted
     edges_to_split = []
     print('Finding edges to split')
     with progressbar.ProgressBar(max_value=len(stations)) as bar:
         for i, station in enumerate(stations):
-            edges_to_split.append(find_edges_to_split(station, nearest_edges[i], streckennetz, s_nodes, s_edges, stations))
+            # if station == 'Tübingen Hbf':
+            edges_to_split.append(find_edges_to_split(station, nearest_edges[i], streckennetz, s_edges, stations, plot=False))
             bar.update(i)
 
     # Find point in edges where the station nodes are inserted
     edges_to_split = tuple(itertools.chain(*edges_to_split))
-    nodes_to_add, edges_to_remove, edges_to_add = split_edges(edges_to_split)
+    pickle.dump(edges_to_split, open("cache/edges_to_split.pkl", "wb"))
+    # edges_to_split = pickle.load(open("cache/edges_to_split.pkl", "rb"))
+
+    print('splitting edges')
+    import math
+    chunksize = math.ceil(len(edges_to_split) / 8)
+    edges_to_split_chunks = [edges_to_split[i:i + chunksize] for i in range(0, len(edges_to_split), chunksize)]
+    with ProcessPoolExecutor(max_workers=8) as executor:
+        result = executor.map(split_edges, edges_to_split_chunks)
+    result = list(result)
+    nodes_to_add = [node for single_result in result for node in single_result[0]]
+    edges_to_remove = [edge for single_result in result for edge in single_result[1]]
+    edges_to_add = [edge for single_result in result for edge in single_result[2]]
+    # result = [item for sublist in result for item in sublist]
+    # nodes_to_add, edges_to_remove, edges_to_add = zip(*result)
+    # nodes_to_add = [item for sublist in nodes_to_add for item in sublist]
+    # edges_to_remove = [item for sublist in edges_to_remove for item in sublist]
+    # edges_to_add = [item for sublist in edges_to_add for item in sublist]
+
     streckennetz.add_nodes_from(nodes_to_add)
     streckennetz.remove_edges_from(edges_to_remove)
     streckennetz.add_edges_from(edges_to_add)
 
-    # nx.write_gpickle(streckennetz, "data_buffer/osm_rail_graph.gpickle")
-
-    # streckennetz = nx.read_gpickle("data_buffer/osm_rail_graph.gpickle")
     # Add length of geometry to edge (osmnx calculates distance from node to node)
     print('Adding more precisely lengths')
     with progressbar.ProgressBar(max_value=len(streckennetz.edges())) as bar:
+        geometries = []
         for i, (u, v) in enumerate(streckennetz.edges()):
             try:
-                streckennetz[u][v][0]['length'] = length_of_line(streckennetz[u][v][0]['geometry'])
+                geometries.append(streckennetz[u][v][0]['geometry'])
             except KeyError:
+                geometries.append(None)
                 pass
             bar.update(i)
-    # nx.write_gpickle(streckennetz, "data_buffer/osm_rail_graph.gpickle")
+        with ProcessPoolExecutor(max_workers=8) as executor:
+                lengths = executor.map(length_of_line, geometries)
+        lengths = [length for length in lengths]
+        for i, (u, v) in enumerate(streckennetz.edges()):
+            if lengths[i] is not None:
+                streckennetz[u][v][0]['length'] = lengths[i]
 
-    upload_minimal(streckennetz)
+    # upload_minimal(streckennetz)
 
     # Test functionality
     path = ox.shortest_path(streckennetz, 'Tübingen Hbf', 'Altingen(Württ)')
