a8d32b8801a41a95173344f9890f951f3ea62577
McToel <theo.doellmann@gmx.de>
2021-03-09 21:42:55 +0100

    Added new Impressum

---
+++ webserver/website/src/views/Impressum.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="impressum">
+    <h1 class="text-center">Impressum</h1>
+    <div>
+      Verantwortlich für diese Seite sind: <br>
+    </div>
+    <div>
+      Theo Döllmann <br>
+      Rappenberghalde 47/3 <br>
+      72070 Tübingen
+    </div>
+    <div>
+      Marius De Kuthy Meurers <br>
+      Weilerhalde 60 <br>
+      72070 Tübingen
+    </div>
+  </div>
+</template>
+<style>
+#impressum {
+  max-width: 800px;
+  width: 90%;
+  margin: auto !important;
+  margin-bottom: 80px !important;
+  display: grid;
+  row-gap: 20px;
+  color: #cacaca;
+}
+</style>
+
+Impressum
+
+Verantwortlich für diese Seite ist:
+
+Thies Clasen
+Rüppurrer Straße 81
+76137 Karlsruhe
