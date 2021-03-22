1904c460e7ef50ae943e1f28131df8df0c2a045f
kuthy <mariusdkm@gmail.com>
2021-03-22 12:08:35 +0100

    Fixed to big date range

--- webserver/website/src/views/stats/Stations.vue
+++ webserver/website/src/views/stats/Stations.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="stats" class="stations">
-    <h1 class="text-center"> Verspätungen in Deutschland </h1>
+    <h1 class="text-center">Verspätungen in Deutschland</h1>
     <div class="stats-picker">
       <vue-slider
         v-model="value"
@@ -10,12 +10,7 @@
         :lazy="true"
         :tooltip="'always'"
       ></vue-slider>
-      <input
-        class="pretty_button"
-        type="button"
-        value="Plot generieren"
-        v-on:click="updatePlot"
-      />
+      <input class="pretty_button" type="button" value="Plot generieren" v-on:click="updatePlot" />
     </div>
     <img
       class="stats-image"
@@ -37,6 +32,7 @@ const date2 = new Date().setHours(0, 0, 0, 0)
 const diffDays = Math.ceil(Math.abs(date2 - date1) / (1000 * 60 * 60))
 const dates = []
 let lastDate = date1
+
 for (let i = 0; i < diffDays; i++) {
   lastDate = new Date(lastDate.getTime() + 60 * 60 * 1000)
   dates.push(
@@ -50,22 +46,16 @@ for (let i = 0; i < diffDays; i++) {
     })
   )
 }
+const randomDate = Math.floor(Math.random() * dates.length)
 export default {
   components: {
     VueSlider
   },
   data: function () {
     return {
-      value: [
-        dates[Math.floor(Math.random() * dates.length)],
-        dates[Math.floor(Math.random() * dates.length)]
-      ],
+      value: [dates[randomDate], dates[randomDate + 84]],
       data: dates,
-      plotURL:
-        window.location.protocol +
-        '//' +
-        window.location.host +
-        '/api/stationplot/default.png'
+      plotURL: window.location.protocol + '//' + window.location.host + '/api/stationplot/default.png'
     }
   },
   methods: {
@@ -90,16 +80,10 @@ export default {
     },
     loaded_img () {
       this.$parent.stop_progress()
-      document
-        .getElementById('stats_image')
-        .scrollIntoView({ behavior: 'smooth' })
+      document.getElementById('stats_image').scrollIntoView({ behavior: 'smooth' })
     },
     replaceByDefault () {
-      this.plotURL =
-        window.location.protocol +
-        '//' +
-        window.location.host +
-        '/api/stationplot/default.png'
+      this.plotURL = window.location.protocol + '//' + window.location.host + '/api/stationplot/default.png'
     }
   }
 }
@@ -125,17 +109,16 @@ export default {
 }
 
 .stats-image {
-    width: 100%;
-    max-width: 50em;
-    margin: auto;
-    display: block;
+  width: 100%;
+  max-width: 50em;
+  margin: auto;
+  display: block;
 }
 
-@media (max-width: 1000px)  {
+@media (max-width: 1000px) {
   .stats-picker {
     flex-wrap: wrap;
   }
-
 }
 
 .vue-slider-process {
