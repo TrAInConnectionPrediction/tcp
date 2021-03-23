a7bb7241d3ca67d3c3adda5265d51fec55dbfa64
McToel <theo.doellmann@gmx.de>
2021-03-23 13:04:58 +0100

    Added short name to manifest

--- webserver/website/vue.config.js
+++ webserver/website/vue.config.js
@@ -2,11 +2,12 @@ module.exports = {
   runtimeCompiler: true,
 
   pwa: {
-    name: 'TCP',
+    name: 'TrAIn_Connection_Prediction',
     themeColor: '#3f51b5',
     msTileColor: '#212121',
     manifestOptions: {
-      background_color: '#212121'
+      background_color: '#212121',
+      short_name: 'TCP'
     },
     assetsVersion: '0.0.1'
   }
