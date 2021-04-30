ec9c5c0a18fb3060dcbae5a7a376fbf08e596285
McToel <theo.doellmann@gmx.de>
2021-04-30 14:12:21 +0200

    Use sqlite for local dev logging

---
+++ webserverconfig.py
@@ -0,0 +1,14 @@
+from config import DB_CONNECT_STRING
+
+class Config(object):
+    # Database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///tcp.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    pass
+
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = DB_CONNECT_STRING
