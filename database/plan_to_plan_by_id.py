07506732f30697a40697850917d5fd3b0e9d48af
McToel <theo.doellmann@gmx.de>
2021-07-02 20:17:29 +0200

    Multithreading

--- database/plan_to_plan_by_id.py
+++ database/plan_to_plan_by_id.py
@@ -6,21 +6,40 @@ from database import PlanById, Session, Plan
 from helpers import StationPhillip
 import sqlalchemy
 from typing import Dict
+import concurrent.futures
+
 
 def plan_to_stops(plan: str, station: str) -> Dict[int, Dict]:
     plan = {int(hash_id): {**plan[hash_id], 'station':station} for hash_id in plan}
     return plan
 
+
+def station_to_by_id(station: str):
+    session: sqlalchemy.orm.Session
+    with Session() as session:
+        plans = Plan.plan_of_station(session, station)
+        plans = [plan_to_stops(plan.plan, station) for plan in plans if plan.plan]
+        parsed = {}
+        for plan in plans:
+            parsed.update(plan)
+        PlanById.add_plan(session, parsed)
+        session.commit()
+
+
 if __name__ == '__main__':
     stations = StationPhillip()
-    for station in tqdm(stations, total=len(stations)):
-        session: sqlalchemy.orm.Session
-        with Session() as session:
-            plans = Plan.plan_of_station(session, station)
-            plans = [plan_to_stops(plan.plan, station) for plan in plans if plan.plan]
-            parsed = {}
-            for plan in plans:
-                parsed.update(plan)
-            PlanById.add_plan(session, parsed)
-            session.commit()
+    with concurrent.futures.ProcessPoolExecutor(min(32, os.cpu_count())) as executor:
+        futures = {executor.submit(station_to_by_id, station): station for station in stations}
+        for future in tqdm(concurrent.futures.as_completed(futures), total=len(stations)):
+            future.result()
+    # for station in tqdm(stations, total=len(stations)):
+    #     session: sqlalchemy.orm.Session
+    #     with Session() as session:
+    #         plans = Plan.plan_of_station(session, station)
+    #         plans = [plan_to_stops(plan.plan, station) for plan in plans if plan.plan]
+    #         parsed = {}
+    #         for plan in plans:
+    #             parsed.update(plan)
+    #         PlanById.add_plan(session, parsed)
+    #         session.commit()
             
