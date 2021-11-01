d0a4cfeb2f04ee3baa665a7c223a28811e786090
McToel <theo.doellmann@gmx.de>
2021-11-01 14:21:21 +0100

    Removed sudo for docker

--- k8s/test_on_server.sh
+++ k8s/test_on_server.sh
@@ -1,7 +1,7 @@
 # Run this script to test your changes on next.trainconnectionprediction.de
 
-sudo DOCKER_BUILDKIT=1 docker build -f webserver/Dockerfile.webserver . -t trainconnectionprediction/next-webserver:latest
+DOCKER_BUILDKIT=1 docker build -f webserver/Dockerfile.webserver . -t trainconnectionprediction/next-webserver:latest
 
-sudo docker push trainconnectionprediction/next-webserver:latest
+docker push trainconnectionprediction/next-webserver:latest
 
 kubectl rollout restart deployment/next-webserver
