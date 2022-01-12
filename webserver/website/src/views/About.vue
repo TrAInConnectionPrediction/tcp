8635cf760841fc43e1f1ef5ab3a31e938ef81e02
McToel <theo.doellmann@gmx.de>
2022-01-12 16:24:23 +0100

    Updated about text

--- webserver/website/src/views/About.vue
+++ webserver/website/src/views/About.vue
@@ -2,14 +2,15 @@
   <div id="about" class="text_content">
     <h1 class="text-center">TrAIn_Connection_Prediction</h1>
     <div>
-      TrAIn_Connection_Prediction ist ein Rankingsystem für Zugverbindungen.
-      Eine auf historischen Zugdaten trainierten KI berechnet für jede
-      Verbindung einen Score, der aussagt wie wahrscheinlich es ist, alle
-      Anschlusszüge zu bekommen. Diese werden dann auf dieser Website farblich
-      kodiert ausgegeben.
+      TrAIn_Connection_Prediction ist ein Rankingsystem für Zugverbindungen. Es kann für alle Zugverbindung innerhalb
+      von Deutschland einen sogenannten Verbindungsscore ermitteln. Dieser sagt aus, wie sicher die Anschlüsse einer
+      Zugverbindung sind. Ein hoher Score heißt, dass vermutlich alle Anschlusszüge erreicht werden, ein niedriger
+      Score bedeutet, dass eventuell nicht alle Anschlusszüge erreicht werden können.
     </div>
+    <div>Ein hoher Verbindungsscore ist aber keine Garantie dafür, dass eine Verbindung funktioniert!</div>
     <div>
-      Ein hoher Verbindungsscore ist aber keine Garantie dafür, dass eine Verbindung funktioniert.
+      Der Verbindungsscore wird mithilfe einer auf historischen Zugdaten trainierten KI ermittelt. Einige Details dazu
+      können in unserem Git-Repository gefunden werden.
     </div>
   </div>
 </template>
