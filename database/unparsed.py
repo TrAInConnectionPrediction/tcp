8a2542f454004f6126cd833b648a603cd116a438
McToel <theo.doellmann@gmx.de>
2022-02-01 23:21:55 +0100

    Retry until no more deadlock

--- database/unparsed.py
+++ database/unparsed.py
@@ -6,9 +6,26 @@ from sqlalchemy import Column, BIGINT, select
 from sqlalchemy.ext.declarative import declarative_base
 from database import get_engine, do_nothing_upsert_base, _gt14
 from typing import List
+import random
+import time
 
 
 Base = declarative_base()
+
+
+def execute_func_that_might_deadlock(func):
+    """
+    Execute a function that might deadlock.
+    """
+    while True:
+        try:
+            return func()
+        except sqlalchemy.exc.OperationalError as e:
+            delay = random.randint(0, 5)
+            print("Failed to fetch db. Wating", delay, "seconds.")
+            print('Failed with exception:')
+            print(e)
+            time.sleep(delay)
 
 
 class UnparsedPlan(Base):
@@ -33,7 +50,8 @@ class UnparsedPlan(Base):
 
     @staticmethod
     def upsert(session: sqlalchemy.orm.Session, rows: List[dict]):
-        return do_nothing_upsert_base(session, UnparsedPlan.__table__, rows)
+        return execute_func_that_might_deadlock(lambda: do_nothing_upsert_base(session, UnparsedPlan.__table__, rows))
+        # return do_nothing_upsert_base(session, UnparsedPlan.__table__, rows)
         
     @staticmethod
     def add(session: sqlalchemy.orm.Session, hash_ids: List[int]) -> None:
@@ -45,12 +63,14 @@ class UnparsedPlan(Base):
     def remove(session: sqlalchemy.orm.Session, hash_ids: List[int]):
         if hash_ids:
             statement = sqlalchemy.delete(UnparsedPlan).where(UnparsedPlan.hash_id.in_(hash_ids))
-            session.execute(statement)
+            execute_func_that_might_deadlock(lambda: session.execute(statement))
+            # session.execute(statement)
 
     @staticmethod
     def remove_all(session: sqlalchemy.orm.Session):
         statement = sqlalchemy.delete(UnparsedPlan)
-        session.execute(statement)
+        execute_func_that_might_deadlock(lambda: session.execute(statement))
+        # session.execute(statement)
     
 
 class UnparsedChange(Base):
@@ -75,7 +95,8 @@ class UnparsedChange(Base):
 
     @staticmethod
     def upsert(session: sqlalchemy.orm.Session, rows: List[dict]):
-        return do_nothing_upsert_base(session, UnparsedChange.__table__, rows)
+        return execute_func_that_might_deadlock(lambda: do_nothing_upsert_base(session, UnparsedChange.__table__, rows))
+        # return do_nothing_upsert_base(session, UnparsedChange.__table__, rows)
 
     @staticmethod
     def add(session: sqlalchemy.orm.Session, hash_ids: List[int]) -> None:
@@ -87,12 +108,14 @@ class UnparsedChange(Base):
     def remove(session: sqlalchemy.orm.Session, hash_ids: List[int]):
         if hash_ids:
             statement = sqlalchemy.delete(UnparsedChange).where(UnparsedChange.hash_id.in_(hash_ids))
-            session.execute(statement)
+            execute_func_that_might_deadlock(lambda: session.execute(statement))
+            # session.execute(statement)
 
     @staticmethod
     def remove_all(session: sqlalchemy.orm.Session):
         statement = sqlalchemy.delete(UnparsedChange)
-        session.execute(statement)
+        execute_func_that_might_deadlock(lambda: session.execute(statement))
+        # session.execute(statement)
 
 
 if __name__ == '__main__':
